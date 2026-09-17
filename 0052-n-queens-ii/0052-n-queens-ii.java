7454541efa442aa5b57d06f26dc7ad2980db6999
Abhishek Bhatia <145684624+abhishekbhatia01@users.noreply.github.com>
2026-09-17 18:03:44 +0530

    Time: 2 ms (54.13%), Space: 42.1 MB (57.44%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    static int cnt = 0;
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] =  '.';
+            }
+        }
+
+        Nqueens(board, 0, n);
+
+        return cnt;
+    }
+
+    public void Nqueens(char[][] board, int row, int n){
+        if(row == n){
+            cnt++;
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(isSafe(board, row, i)){
+                board[row][i] = 'Q';
+                Nqueens(board, row+1, n);
+                board[row][i] = '.';
+            }
+        }
+    }
+
+    public boolean isSafe(char[][] board, int row, int col){
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<String> construct(char[][] board){
+        List<String> res = new ArrayList<>();
+
+        for(char[] ch : board){
+            res.add(new String(ch));
+        }
+
+        return res;
+    }
+}
